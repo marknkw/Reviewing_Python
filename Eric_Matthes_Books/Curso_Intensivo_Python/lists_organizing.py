9944eed3bc3aabffde303ddecf636515ffe99afa
marknkw <mark.s.g@hotmail.com>
2022-09-15 12:53:55 -0300

    Oganizando_Uma_Lista
    Organizando uma lista; funções e métodos

---
+++ Eric_Matthes_Books/Curso_Intensivo_Python/lists_organizing.py
@@ -0,0 +1,26 @@
+#O método .sort() oferece uma forma de alterar a ordem de uma lista e armazenar os itens, nessa mesma lista, em ordem alfabética
+#Ex: list = ['b', 't', 'k', 'l']
+# list.sort()
+#print(list)
+#output: ['b', 'k', 'l', 't']
+#Também é possível adicionar argumentos ao método .sort(), como o o argumento "reverse=True"
+#No qual a lista será ordenada de forma inversa
+#Ex: list = ['b', 't', 'k', 'l']
+# list.sort(reverse = True)
+#print(list)
+#output: ['t', 'l', 'k', 'b']
+
+#A função sorted() permite exibir uma lista sem afetar a ordem permanente da variável de lista na memória
+#Ou seja, os itens são organizados sem alterar, permanentemente, a lista exibida.
+#O argumento "reverse = True" também é aceito
+
+#O método .reverse() inverte a ordem original de uma lista.
+#Ex: list = ['b', 't', 'k', 'l']
+# list.reverse()
+#print(list)
+#output: ['l', 'k', 't', 'b']
+
+#A função len(lista_qualquer) retorna o tamanho do tamanho de uma lista
+#Ex: list = ['b', 't', 'k', 'l']
+#print(len(list))
+#output: 4
